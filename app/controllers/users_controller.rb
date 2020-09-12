6be5c9342c17790281a637431e1539eb1d4e668c
WaredDeveloper <waredrubydeveloper@gmail.com>
2020-09-12 15:26:14 +0300

    Add post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 
   end
 
@@ -18,6 +20,11 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -49,14 +56,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     def correct_user
